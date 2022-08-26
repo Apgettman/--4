5f9b367cfed8c2ff018cfb654a2ec8dd6543cc84
679 <apgettman@gmail.com>
2022-08-26 21:28:03 +0300

    ДЗ-4

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // Задание 1
-        byte cat = 1;
+        byte cat = 2;
         short mouse = 3;
         int dog = 7;
         long elephant = 110L;
